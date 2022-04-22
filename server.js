14a47bfb547a7a45df2c1c35b052eb322e42a0ea
Shawn Khandia <shawnk04@email.unc.edu>
2022-04-21 23:21:48 -0400

    Formatting changes

--- server.js
+++ server.js
@@ -25,6 +25,30 @@ args['port'];
 const port = args.port || process.env.PORT || 3000;
 
 
+/*
+    HELP
+*/
+// Define help constant
+const help = (`
+server.js [options]
+--port, -p	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+--debug, -d If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help, -h	Return this message and exit.
+`);
+
+// If --help is called, print help and exit
+if (args.help || args.h) {
+    console.log(help);
+    process.exit(0);
+}
+
+
 /* 
     COIN FUNCTIONS 
 */
@@ -130,26 +154,3 @@ app.listen(port, () => {
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
-
-/*
-    HELP
-*/
-// Define help constant
-const help = (`
-server.js [options]
---port, -p	Set the port number for the server to listen on. Must be an integer
-            between 1 and 65535.
---debug, -d If set to true, creates endlpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error which throws 
-            an error with the message "Error test successful." Defaults to 
-            false.
---log		If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
---help, -h	Return this message and exit.
-`);
-
-// If --help is called, print help and exit
-if (args.help || args.h) {
-    console.log(help);
-    process.exit(0);
-}
